502aac457e691d62b5447156f22c3f08d06175d6
Syrie Bianco <syriebianco@gmail.com>
2017-06-29 03:18:55 -0700

    Complete phase 4.4

---
+++ skeleton/phase_4_recursion.js
@@ -0,0 +1,52 @@
+// #1 range
+
+function range(start, end) {
+  if (start === end) {
+    return [];
+  }
+
+  let result = range(start, end - 1)
+  result.push(end - 1)
+  return result
+}
+
+range(3,5)
+
+// #2 recursive sum
+
+function recSum(arr) {
+  if (arr.length < 1) { return 0; }
+
+  let sum = recSum(arr.slice(0, -1)) + arr[arr.length - 1]
+  return sum
+  console.log(arr)
+}
+
+recSum([1, 2, 3])
+
+// #3 exponent 1
+
+function exponent1(base, exp) {
+  if (exp === 0) { return 1 }
+
+  let result = base * exponent1(base, exp - 1)
+  return result
+}
+
+exponent1(5, 3)
+
+// #4 exponent2
+
+function exponent2(base, exp) {
+  if (exp === 0) { return 1 }
+  if (exp === 1) { return base }
+
+  if(exp % 2 === 0){
+    return exponent2(base, exp/2) * exponent2(base, exp/2)
+  } else {
+    return base *
+    (exponent2(base, (exp - 1) / 2) * exponent2(base, (exp - 1) / 2))
+  }
+}
+
+exponent2(5, 3)
