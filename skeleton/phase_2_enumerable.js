94df149b8c8bea8714a3db4b146ef05b80cf7940
Syrie Bianco <syriebianco@gmail.com>
2017-06-28 18:36:52 -0700

    Attempt phase 2.2

--- skeleton/phase_2_enumerable.js
+++ skeleton/phase_2_enumerable.js
@@ -8,3 +8,16 @@ Array.prototype.myEach = function(callback) {
 
 
 // #2 my map
+// why this no worky?
+
+Array.prototype.myMap = function (callback) {
+  result = []
+  this.myEach(result.push(callback))
+  return result
+}
+
+function plusTwo(num) { (num + 2) }
+
+const arr = [1, 2, 3, 4]
+
+arr.myMap(plusTwo)
